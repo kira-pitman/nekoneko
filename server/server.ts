a0879df191a1abe1896bcf1f8b312bebc1f3e6b2
Kira Pitman <kirapitman@gmail.com>
2023-10-30 14:33:43 +1300

    write server.ts route for neko

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,10 @@
 import * as Path from 'node:path'
-
 import express from 'express'
-
+import nekoRoute from './routes/neko-routes'
 
 const server = express()
 server.use(express.json())
+server.use('/api/v1/neko', nekoRoute)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
